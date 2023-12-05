88e812d2c870e45072685c5a63d66dd8995ddc8e
Alan <alanmurosdlg@gmail.com>
2023-12-04 23:43:46 -0800

    header fix2

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-[#052E16] w-full h-[70px] p-3 px-16  text-white text-center">
+    <header className="bg-[#052E16] w-full h-[60px] p-3 text-white text-center">
       <div className="flex flex-row justify-between items-center">
         <h1 className="text-2xl font-bold ">SIE</h1>
         <img
